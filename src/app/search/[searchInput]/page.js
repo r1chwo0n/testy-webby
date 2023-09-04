dec3c4763a6bab875a88c1cc6cb91646e6212e4f
r1chwo0n <woonzy09@gmail.com>
2023-09-04 21:41:03 +0700

    first commit

---
+++ src/app/search/[searchInput]/page.js
@@ -0,0 +1,50 @@
+"use client";
+
+import { MovieRow } from "@/components/MovieRow";
+import { movieDB } from "@/libs/movieDB";
+
+export default function SearchResultPage({ params }) {
+  const searchInput = params.searchInput;
+  const processedSearchInput = searchInput.replaceAll("%20", " ");
+  const filteredMovies = movieDB.filter((movie) =>
+    movie.title
+      .toLocaleLowerCase()
+      .includes(processedSearchInput.toLocaleLowerCase())
+  );
+
+  //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
+  // const processedSearchInput = ...
+
+  /*
+  tip2 : Use "includes" string method to check substring
+  Example : "ABC".includes("AB") -> return true
+
+  tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
+  to convert movie title and searchInput to lower case 
+  const filteredMovies = movieDB.filter((movie) =>
+    you code here...
+  );
+  */
+
+  return (
+    <div>
+      <p className="fw-bold fs-4 text-center my-0">
+        Searching &quot; {processedSearchInput} &quot;
+      </p>
+      <p className="fw-bold fs-4 text-center">
+        Found {filteredMovies.length} result(s)
+      </p>
+      {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {filteredMovies.map((movie, i) => (
+        <MovieRow
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          detail={movie.detail}
+          rating={movie.rating}
+          number={i + 1}
+        />
+      ))}
+    </div>
+  );
+}

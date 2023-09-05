8db5846a487aea59e1a171171080dea58699ee2b
r1chwo0n <woonzy09@gmail.com>
2023-09-05 08:31:09 +0700

    second

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Lab13",
+  title: "Water Strip",
   description: "Next13 Routing",
 };
 
@@ -20,11 +20,12 @@ export default function RootLayout({ children }) {
           integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
           crossOrigin="anonymous"
         />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
       </head>
       <body className={inter.className}>
-        <div style={{ maxWidth: "600px" }} className="mx-auto">
-          <Header />
+        <div className="mx-auto">
           <Navbar />
+          <Header />
           {children}
           <Footer
             year="2023"

5f2fb5d0505cb0e47de6a1fd11b657d22d7e04d7
r1chwo0n <woonzy09@gmail.com>
2023-09-05 14:08:01 +0700

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer year="2023" fullName="Nawawan" studentId="650610775" />
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script> */}
       </body>
     </html>
   );
